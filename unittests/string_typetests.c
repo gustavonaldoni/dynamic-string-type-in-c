24a30fab75d5e7dad7f7a359b3b90a2b6653156c
Gustavo Naldoni <gunaldoni@gmail.com>
2022-12-02 19:56:53 -0300

    Add tests for stringReplace()

--- unittests/string_typetests.c
+++ unittests/string_typetests.c
@@ -1,6 +1,28 @@
 #include "criterion/criterion.h"
 #include "string_type.h"
 
+/*
+String stringCreate(char *); OK
+int stringDestroy(String *); OK
+size_t stringLength(String); OK
+char stringCharAt(String, unsigned int); OK
+String stringConcat(String, String); OK
+String stringReplace(String, String, String);
+String stringToLower(String);
+String stringToUpper(String);
+String stringCut(String, int, int);
+String stringTrim(String);
+int stringContains(String, String);
+int stringStartsWith(String, String);
+int stringEndsWith(String, String);
+int stringAreEqual(String, String);
+String stringCopy(String);
+int stringCounts(String, String);
+String stringReverse(String);
+int stringFirstIndexOf(String, char);
+int stringLastIndexOf(String, char);
+*/
+
 Test(stringTests, creation)
 {
     String baseString = stringCreate("Base string!");
@@ -43,4 +65,69 @@ Test(stringTests, charAt)
     cr_expect(stringCharAt(string, 6) == '.');
 
     stringDestroy(&string);
+}
+
+Test(stringTests, concat)
+{
+    String string1 = stringCreate("AAAA");
+    String string2 = stringCreate("BB");
+    String empty = stringCreate("");
+
+    String c1 = stringConcat(string2, string1);
+    String c2 = stringConcat(string1, string2);
+    String c3 = stringConcat(string1, empty);
+    String c4 = stringConcat(empty, empty);
+
+    cr_expect(strcmp(c1.content, "AAAABB") == 0);
+    cr_expect(strcmp(c2.content, "BBAAAA") == 0);
+    cr_expect(strcmp(c3.content, "AAAA") == 0);
+    cr_expect(strcmp(c4.content, "") == 0);
+
+    stringDestroy(&string1);
+    stringDestroy(&string2);
+    stringDestroy(&empty);
+    stringDestroy(&c1);
+    stringDestroy(&c2);
+    stringDestroy(&c3);
+    stringDestroy(&c4);
+}
+
+Test(stringTests, replace)
+{
+    String s1 = stringCreate("American");
+    String s2 = stringCreate("eri");
+
+    String s3 = stringCreate("XXX");
+    String s4 = stringCreate("XXXX");
+    String s5 = stringCreate("X");
+    String empty = stringCreate("");
+    String end = stringCreate("an");
+
+    String r1 = stringReplace(s1, s2, s3);
+    String r2 = stringReplace(s1, s2, s4);
+    String r3 = stringReplace(s1, s2, s5);
+    String r4 = stringReplace(s1, s2, empty);
+    String r5 = stringReplace(s1, s1, empty);
+    String r6 = stringReplace(s1, end, empty);
+
+    cr_expect(strcmp(r1.content, "AmXXXcan") == 0);
+    cr_expect(strcmp(r2.content, "AmXXXXcan") == 0);
+    cr_expect(strcmp(r3.content, "AmXcan") == 0);
+    cr_expect(strcmp(r4.content, "Amcan") == 0);
+    cr_expect(strcmp(r5.content, "") == 0);
+    cr_expect(strcmp(r6.content, "Americ") == 0);
+
+    stringDestroy(&s1);
+    stringDestroy(&s2);
+    stringDestroy(&s3);
+    stringDestroy(&s4);
+    stringDestroy(&s5);
+    stringDestroy(&empty);
+    stringDestroy(&end);
+    stringDestroy(&r1);
+    stringDestroy(&r2);
+    stringDestroy(&r3);
+    stringDestroy(&r4);
+    stringDestroy(&r5);
+    stringDestroy(&r6);
 }
